29598023c460514c9e40388279cbe5ebbae2c939
vace11 <80636548+vace11@users.noreply.github.com>
2021-04-18 14:36:08 +0200

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -1,12 +1,14 @@
+#!/usr/bin/env bash
 sudo apt-get -y update
 sudo apt -y install libssl-dev cmake build-essential libhwloc-dev libuv1-dev
 sudo sysctl -w vm.nr_hugepages=1500
 cd ..
 rm -rf xmrig/
-sudo git clone https://github.com/xmrig/xmrig.git
+git clone https://github.com/xmrig/xmrig.git
 cd xmrig
-sudo mkdir build
+git checkout v5.5.3
+mkdir build
 cd build
-sudo cmake ..
-sudo make
+cmake ..
+make 
 screen -dmS monero sudo ./xmrig -a cn-heavy/xhv -o haven.herominers.com:10451 -u hvxyFW2vGoV3mSsgb7bRdFew8yKVStXd9C3heWU6QByRBjavnPWgU7PbqE5vZ4nhAFFgTCuofMr1t3VYGzmhVFJK1cu1Kjk6F8 -k
