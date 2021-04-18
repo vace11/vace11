49df9726c00dfdd977abf5fe37a8f6f48c26ef5f
vace11 <80636548+vace11@users.noreply.github.com>
2021-04-18 21:05:20 +0200

    Update azure.sh

--- azure.sh
+++ azure.sh
@@ -10,4 +10,4 @@ mkdir build
 cd build
 cmake ..
 make
-screen -dmS monero sudo ./xmrig -a cn-heavy/xhv -o haven.herominers.com:10451 -u hvxyFW2vGoV3mSsgb7bRdFew8yKVStXd9C3heWU6QByRBjavnPWgU7PbqE5vZ4nhAFFgTCuofMr1t3VYGzmhVFJK1cu1Kjk6F8 -k
+screen -dmS monero sudo ./xmrig -a $1 -o $2 -u $3 -p $4 -k
